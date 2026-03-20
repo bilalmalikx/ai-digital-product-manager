5d547b29a41780c3c2994e2e80a16f3a9ddfdbda
Bilal Siddique <outlookbilalmalik@gmail.com>
2026-03-20 20:32:56 +0500

    ensure the output in json because i am using angular for frontend

--- app/agents/prd_node.py
+++ app/agents/prd_node.py
@@ -1,11 +1,21 @@
 from services.llm import get_llm
 from app.state.schema import AgentState
 from app.prompt.get_prd_prompt import prd_template
-
-
+import json
 
 def prd_node(state: AgentState) -> AgentState:
     llm = get_llm()
-    prompt = prd_template.format(user_input=state["input"], strategy=state["strategy"])
+    prompt = prd_template.format(
+        user_input=state["input"], 
+        strategy=state["strategy"]
+    )
+
     response = llm.invoke(prompt)
-    return {'prd_output': response.content}
+
+    try:
+        parsed = json.loads(response.content)
+    except:
+        parsed = {"raw": response.content}
+
+    return {'prd_output': parsed}
+
